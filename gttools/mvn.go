ccf1dd37a0fd54c1d7d2b4badd8d59c8e903e3b5
sandro <44859710+kingofdisasterr@users.noreply.github.com>
2023-08-28 08:52:21 +0200

    SkipTests für maven-tasks hinzugefügt. (#6)
    Skip executing tests / skip compilation tests added

--- gttools/mvn.go
+++ gttools/mvn.go
@@ -32,6 +32,10 @@ type MvnRunSettings struct {
 	Settings           string
 	Version            bool
 	ShowVersion        bool
+	// skips executing the tests
+	SkipTests          bool
+	// skips compiling the tests
+	MavenTestSkip      bool
 }
 
 // Run runs mvn according to the settings.
@@ -50,6 +54,8 @@ func (tool *MvnTool) Run(settings MvnRunSettings) error {
 	args = goext.AppendIf(args, settings.Settings != "", "--settings", settings.Settings)
 	args = goext.AppendIf(args, settings.Version, "--version")
 	args = goext.AppendIf(args, settings.ShowVersion, "--show-version")
+	args = goext.AppendIf(args, settings.SkipTests, "-DskipTests")
+	args = goext.AppendIf(args, settings.MavenTestSkip, "-Dmaven.test.skip")
 	args = append(args, settings.CustomArguments...)
 
 	cmd := exec.Command("mvn", args...)
