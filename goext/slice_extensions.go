deb0e85e67d7dc93fd7966eb1007d844e2382825
Roman Baeriswyl <r.baeriswyl@gmail.com>
2024-02-22 22:22:41 +0100

    Added Flyway tool

--- goext/slice_extensions.go
+++ goext/slice_extensions.go
@@ -1,8 +1,22 @@
 package goext
 
+import (
+	"fmt"
+	"strings"
+)
+
 // AppendIf adds the given values to a slice if the condition is fulfilled.
 func AppendIf[T any](slice []T, cond bool, values ...T) []T {
 	if cond {
+		return append(slice, values...)
+	}
+	return slice
+}
+
+// Appends the given value if the condition is fulfilled. Calculates the value to add only if the condition is fulfulled.
+func AppendIfFunc[T any](slice []T, cond bool, f func() []T) []T {
+	if cond {
+		values := f()
 		return append(slice, values...)
 	}
 	return slice
@@ -37,7 +51,16 @@ func ChunkBy[T any](items []T, chunkSize int) (chunks [][]T) {
 	return append(chunks, items)
 }
 
-// Prepend prepends the given elements to the given array
+// Prepend prepends the given elements to the given array.
 func Prepend[T any](slice []T, elems ...T) []T {
 	return append(elems, slice...)
+}
+
+// Joins any slice with the given separator.
+func StringsJoinAny[T any](elems []T, sep string) string {
+	values := []string{}
+	for _, element := range elems {
+		values = append(values, fmt.Sprintf("%v", element))
+	}
+	return strings.Join(values, sep)
 }
