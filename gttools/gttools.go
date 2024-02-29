563d4f9fe17579a06ebcae6368ae8df4ef137041
Roman Bäriswyl <r.baeriswyl@gmail.com>
2024-02-29 15:13:32 +0100

    Added initial Dev Container CLI tool

--- gttools/gttools.go
+++ gttools/gttools.go
@@ -8,31 +8,33 @@ import (
 
 // ToolsClient provides typed access to the different tools.
 type ToolsClient struct {
-	Cypress *CypressTool
-	Docker  *DockerTool
-	DotNet  *DotNetTool
-	EsLint  *EsLintTool
-	Flyway  *FlywayTool
-	GitLab  *GitLabTool
-	JFrog   *JFrogTool
-	Mvn     *MvnTool
-	Npm     *NpmTool
-	Nx      *NxTool
+	Cypress         *CypressTool
+	DevContainerCli *DevContainerCliTool
+	Docker          *DockerTool
+	DotNet          *DotNetTool
+	EsLint          *EsLintTool
+	Flyway          *FlywayTool
+	GitLab          *GitLabTool
+	JFrog           *JFrogTool
+	Mvn             *MvnTool
+	Npm             *NpmTool
+	Nx              *NxTool
 }
 
 // CreateToolsClient creates a new client to access the different tools.
 func CreateToolsClient() *ToolsClient {
 	return &ToolsClient{
-		Cypress: CreateCypressTool(),
-		Docker:  CreateDockerTool(),
-		DotNet:  CreateDotNetTool(),
-		EsLint:  CreateEsLintTool(),
-		Flyway:  CreateFlywayTool(),
-		GitLab:  CreateGitLabTool(),
-		JFrog:   CreateJFrogTool(),
-		Mvn:     CreateMvnTool(),
-		Npm:     CreateNpmTool(),
-		Nx:      CreateNxTool(),
+		Cypress:         CreateCypressTool(),
+		DevContainerCli: CreateDevContainerCliTool(),
+		Docker:          CreateDockerTool(),
+		DotNet:          CreateDotNetTool(),
+		EsLint:          CreateEsLintTool(),
+		Flyway:          CreateFlywayTool(),
+		GitLab:          CreateGitLabTool(),
+		JFrog:           CreateJFrogTool(),
+		Mvn:             CreateMvnTool(),
+		Npm:             CreateNpmTool(),
+		Nx:              CreateNxTool(),
 	}
 }
 
